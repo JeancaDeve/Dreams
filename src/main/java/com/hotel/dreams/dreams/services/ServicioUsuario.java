c144b4cfeead74602470bac69864e504a5ed6791
JeancaDeve <khevinelpro@gmail.com>
2024-04-10 10:38:21 -0500

    Se agrego un nueva funcion al contarto de Usuario

--- src/main/java/com/hotel/dreams/dreams/services/ServicioUsuario.java
+++ src/main/java/com/hotel/dreams/dreams/services/ServicioUsuario.java
@@ -3,5 +3,7 @@ package com.hotel.dreams.dreams.services;
 import com.hotel.dreams.dreams.entities.Usuario;
 
 public interface ServicioUsuario extends ServicioBase<Usuario, Integer>{
-
+    //metodo para guardar de manera encriptada la informacion sensible del usuario
+    //usaremos argon2 u otro algoritmo
+    public Usuario registrarUsuario(Usuario usuario);
 }
---
+++ src/main/java/com/hotel/dreams/dreams/services/impl/ServicioImplUsuario.java
@@ -0,0 +1,29 @@
+package com.hotel.dreams.dreams.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotel.dreams.dreams.entities.Usuario;
+import com.hotel.dreams.dreams.repositories.RepositorioBase;
+import com.hotel.dreams.dreams.repositories.RepositorioUsuario;
+import com.hotel.dreams.dreams.services.ServicioUsuario;
+
+@Service
+public class ServicioImplUsuario extends ServicioBaseImpl<Usuario, Integer> implements ServicioUsuario {
+
+    @Autowired
+    RepositorioUsuario repositorioBase;
+
+    public ServicioImplUsuario(RepositorioBase<Usuario, Integer> repositorioBase) {
+        super(repositorioBase);
+
+    }
+
+
+    //todo: completar el metodo, aun falta darle funcionamiento
+    @Override
+    public Usuario registrarUsuario(Usuario usuario) {
+        throw new UnsupportedOperationException("Unimplemented method 'registrarUsuario'");
+    }
+
+}
