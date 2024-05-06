cf66e06dcfd3fa9c41e9bf788ea3eb86962d51fe
JeancaDeve <khevinelpro@gmail.com>
2024-05-06 15:00:58 -0500

    Se agrego nueva funcion para bsucar por id al huesped

--- src/main/java/com/hotel/dreams/dreams/repositories/RepositorioHuesped.java
+++ src/main/java/com/hotel/dreams/dreams/repositories/RepositorioHuesped.java
@@ -3,8 +3,9 @@ package com.hotel.dreams.dreams.repositories;
 import org.springframework.stereotype.Repository;
 
 import com.hotel.dreams.dreams.models.Huesped;
+import java.util.Optional;
 
 @Repository
 public interface RepositorioHuesped extends RepositorioBase<Huesped, Integer> {
-
+  public Optional<Huesped> findByDni(String numeroCelular);
 }
