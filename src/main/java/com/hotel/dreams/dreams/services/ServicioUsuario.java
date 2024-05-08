201088313487edb4d4609cfd4c4c70b67151113e
JeancaDeve <khevinelpro@gmail.com>
2024-05-08 10:17:21 -0500

    Se eliminaron entidades, etc , que no se estan usando

--- src/main/java/com/hotel/dreams/dreams/services/ServicioUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.hotel.dreams.dreams.services;
-
-import com.hotel.dreams.dreams.models.Usuario;
-
-public interface ServicioUsuario extends ServicioBase<Usuario, Integer>{
-
-    public Usuario registrarUsuario(Usuario usuario);
-}
