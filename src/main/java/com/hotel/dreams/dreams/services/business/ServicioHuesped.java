278fcf77a3f06d52572e59ae9ac3d538ca8073f7
JeancaDeve <khevinelpro@gmail.com>
2024-05-15 19:47:50 -0500

    nuevo metodo

--- src/main/java/com/hotel/dreams/dreams/services/business/ServicioHuesped.java
+++ src/main/java/com/hotel/dreams/dreams/services/business/ServicioHuesped.java
@@ -3,5 +3,7 @@ package com.hotel.dreams.dreams.services.business;
 import com.hotel.dreams.dreams.models.Huesped;
 
 public interface ServicioHuesped extends ServicioBase<Huesped, Integer> {
-   public String hacerReserva(Huesped huesped, int idHabitacion) throws Exception;
+   public void hacerReserva(Huesped huesped) throws Exception;
+
+   public Huesped buscarPorDni(String numeroDni) throws Exception;
 }
