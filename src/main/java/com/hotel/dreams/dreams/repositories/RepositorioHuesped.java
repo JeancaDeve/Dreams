a00b35eda2cfc7df303e67ecad4f23a10955e1ef
JeancaDeve <khevinelpro@gmail.com>
2024-05-15 19:47:35 -0500

    Nuevos metodos

--- src/main/java/com/hotel/dreams/dreams/repositories/RepositorioHuesped.java
+++ src/main/java/com/hotel/dreams/dreams/repositories/RepositorioHuesped.java
@@ -1,5 +1,7 @@
 package com.hotel.dreams.dreams.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hotel.dreams.dreams.models.Huesped;
@@ -7,5 +9,18 @@ import java.util.Optional;
 
 @Repository
 public interface RepositorioHuesped extends RepositorioBase<Huesped, Integer> {
-  public Optional<Huesped> findByDni(String numeroCelular);
+
+  public Optional<Huesped> findByDni(String numeroDni);
+
+  // metodo para buscar una entidad por todos su atributos menos el id
+  @Query("SELECT h FROM Huesped h WHERE " +
+      " h.nombres = :#{#huesped.nombres}" +
+      " AND h.apellidos = :#{#huesped.apellidos} " +
+      " AND h.numeroCelular = :#{#huesped.numeroCelular} " +
+      " AND h.dni = :#{#huesped.dni} " +
+      " AND h.fechaNacimiento = :#{#huesped.fechaNacimiento}")
+  public Optional<Huesped> findByAll(@Param("huesped") Huesped huesped);
+
+  public Optional<Huesped> findByNumeroCelular(String numeroCelular);
+
 }
